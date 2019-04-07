62985bb79621b797d74b470924415955b92d7562
Gil Fuser <gilfuser@gmail.com>
2019-04-06 22:07:23 -0300

    dotenv wont work

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ module.exports = {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // '@nuxtjs/pwa',
-    '@nuxtjs/dotenv',
+    // '@nuxtjs/dotenv',
     '@nuxtjs/vuetify'
   ],
   /*
@@ -129,12 +129,13 @@ module.exports = {
   dev: process.env.NODE_ENV === 'DEV',
 
   env: {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    WS_URL: process.env.WS_URL
+    apiKey: 'AIzaSyD4uQGKCqb0yQGSnRrLIIgp1KsMC4EDtK4',
+    authDomain: 'deslugarejo-db.firebaseapp.com',
+    databaseURL: 'https://deslugarejo-db.firebaseio.com',
+    projectId: 'deslugarejo-db',
+    storageBucket: 'deslugarejo-db.appspot.com',
+    messagingSenderId: '956505351808',
+    clientId: '956505351808-ko0d8o5aptpku1o9sp6sofj3j6lkdo5d.apps.googleusercontent.com',
+    WS_URL: process.env.WS_URL || 'http://localhost:3000'
   }
 }
